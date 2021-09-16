8ee89c074cb26b3e97be674b3b5224c1e759fcff
marc572m <89967363+marc572m@users.noreply.github.com>
2021-09-16 19:30:30 +0200

    Update Main.java
    Better

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,5 @@
 package com.company;
 
-import java.sql.SQLOutput;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -11,33 +10,38 @@ public class Main {
             System.out.println("The answer is lower");
         } else if (a > b){
             System.out.println("The answer is higer");
-        }else
+        }else {
             System.out.println("You guessed the number!");
-        return;
+            System.exit(0);
+        }
     }
-
+    
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         Random rdm = new Random();
         int number = rdm.nextInt(100) + 1;
         System.out.println("I'm thinking of a number between 1-100 (including both)" +
                 "\nCan you guess what it is? \nEnter a number:");
-        int guess = in.nextInt();
+        try {
+            //first guess
+            int guess = in.nextInt();
+            System.out.println("Your guess is: " + guess + number);
+            answer(number, guess);
 
-        //first guess
-        System.out.println("Your guess is: " + guess);
-        answer(number, guess);
+            //another try
+            System.out.println("Try again");
+            int guess2 = in.nextInt();
+            answer(number, guess2);
 
-        //another try
-        System.out.println("Try again");
-        int guess2 = in.nextInt();
-        answer(number, guess2);
-
-        //last try + answer in the end
-        System.out.println("Last try");
-        int guess3 = in.nextInt();
-        answer(number, guess3);
-        System.out.println("Bad luck the answer is: " + number);
+            //last try + answer in the end
+            System.out.println("Last try");
+            int guess3 = in.nextInt();
+            answer(number, guess3);
+            System.out.println("Bad luck the answer is: " + number);
+        }
+        catch (Exception e){
+            System.err.println("Needs to be a number!");
+        }
 
     }
 }
